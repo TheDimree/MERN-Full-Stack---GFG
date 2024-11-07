ff0bb6f94b4d06a267bc6d0d3b6d08afcf5e1105
Bhagesh Dimri <TheDimree@gmail.com>
2024-11-07 17:51:11 +0530

    movie-app JS Project completed

--- 2.javascript/Projects/AdvancedProjects/movie-app/src/index.js
+++ 2.javascript/Projects/AdvancedProjects/movie-app/src/index.js
@@ -1,11 +1,18 @@
 // const URL = "https://movies-app.prakashsakari.repl.co/api/movies"
 // const URL = "https://dummyjson.com/products";
-const URL = "http://localhost:8008/movies"
+const URL = "http://localhost:8008/movies";
 const parentElement = document.querySelector(".main");
 const createElement = (element) => document.createElement(element);
-const searchInput = document.querySelector(".input")
+const searchInput = document.querySelector(".input");
+const movieRatings = document.querySelector("#rating-select");
+const moviesGenre = document.querySelector("#genre-select");
 
-let searchValue ="", movies, filteredArrayOfMovies = [];
+let searchValue = "",
+  movies,
+  filteredArrayOfMovies = [],
+  ratings = 0,
+  genres = [],
+  genre = ""; //selected on the dropdown
 
 const getMovies = async (url) => {
   // * Traditional Way: Fetch method
@@ -36,7 +43,8 @@ const createMovieCard = (movies) => {
     "grid-cols-1",
     "sm:grid-cols-2",
     "lg:grid-cols-4",
-    "gap-6", "my-2"
+    "gap-6",
+    "my-2"
   );
 
   for (let movie of movies) {
@@ -109,22 +117,106 @@ const createMovieCard = (movies) => {
 
 (async () => {
   await getMovies(URL);
+
+  // getting all unique genres from api's genre variable
+  genres = movies.reduce((accumulator, current) => {
+    let genreArr = []; // will store finally unique genres
+    let tempGenreArr = current.genre.split(",");
+    // console.log(tempGenreArr);
+    accumulator = [...accumulator, ...tempGenreArr];
+    for (let genre of accumulator) {
+      if (!genreArr.includes(genre)) {
+        // genreArr.push(genre) // * directly mutates the original array by adding genre to the end
+        genreArr = [...genreArr, genre]; // * creates a new array each time with the contents of genreArr plus the new genre
+      }
+    }
+    return genreArr;
+  }, []); // accumulator  is this array and current will be the every single object, [] is also the initial value as we will return the array.
+  // console.log(genres);
+
+  // Listing Genres in dropdown
+  for (let genre of genres) {
+    const option = createElement("option");
+    option.classList.add("option");
+    option.setAttribute("value", genre);
+    option.innerText = genre;
+    moviesGenre.appendChild(option);
+  }
 })();
 
+function getFilteredData(searchValue) {
+  filteredArrayOfMovies =
+    searchValue?.length > 0
+      ? movies.filter(
+        (movie) =>
+          (typeof movie.name === "string" &&
+            movie.name.toLowerCase().includes(searchValue)) ||
+          (typeof movie.director_name === "string" &&
+            movie.director_name.toLowerCase().includes(searchValue)) ||
+          (typeof movie.cast_name === "string" &&
+            movie.cast_name.toLowerCase().includes(searchValue))
+      )
+      : movies;
+  if (ratings > 0) {
+    filteredArrayOfMovies =
+      searchValue?.length > 0 || genre != "" ? filteredArrayOfMovies : movies;
+    filteredArrayOfMovies = filteredArrayOfMovies.filter(
+      (movie) => movie.imdb_rating >= ratings
+    );
+  }
 
-
-function handleSearch(event) {
-    searchValue = event.target.value.toLowerCase();
-    // console.log(searchValue)
-
-    filteredArrayOfMovies = searchValue?.length > 0 ? movies.filter(movie => (typeof movie.name === 'string' && movie.name.toLowerCase().includes(searchValue)) || (typeof movie.director_name === 'string' && movie.director_name.toLowerCase().includes(searchValue)) || (typeof movie.cast_name === 'string' && movie.cast_name.toLowerCase().includes(searchValue))) : movies;
-    // console.log(filteredArrayOfMovies)
-     
-    parentElement.innerHTML = ""
-    createMovieCard(filteredArrayOfMovies)
-
+  if (genre?.length > 0) {
+    filteredArrayOfMovies =
+      searchValue?.length > 0 || ratings > 7 ? filteredArrayOfMovies : movies;
+    filteredArrayOfMovies = filteredArrayOfMovies.filter((movie) =>
+      movie.genre.includes(genre)
+    );
+  }
+  console.log("Filtered movies: ", filteredArrayOfMovies);
+  console.log("searchValue: ", searchValue);
+  console.log("ratings: ", ratings);
+  console.log("genre: ", genre);
+  return filteredArrayOfMovies;
 }
 
-searchInput.addEventListener("keyup", handleSearch) // check for "change"=> you have to wait and click outside or enter.
+function handleSearch(event) {
+  searchValue = event.target.value.toLowerCase();
+  // console.log(searchValue)
+  let filteredBySearch = getFilteredData(searchValue);
+  parentElement.innerHTML = "";
+  createMovieCard(filteredBySearch);
+}
 
-// -46:23
+function debounce(callback, delay) {
+  let timerId;
+
+  return (...args) => {
+    clearTimeout(timerId);
+    timerId = setTimeout(() => {
+      callback(...args);
+    }, delay);
+  };
+}
+
+const debounceInput = debounce(handleSearch, 500);
+
+function handleRatingSelector(event) {
+  ratings = event.target.value;
+  console.log("ratings=", ratings);
+  let filterByRating = getFilteredData(searchValue);
+  parentElement.innerHTML = "";
+  createMovieCard(ratings ? filterByRating : movies);
+}
+
+function handleGenreSelect(event) {
+  genre = event.target.value;
+  console.log("Genre selected: ", genre);
+  const filterByGenre = getFilteredData(searchValue);
+  console.log("Genre total: ", filterByGenre.length);
+  parentElement.innerHTML = "";
+  createMovieCard(genre ? filterByGenre : movies);
+}
+
+searchInput.addEventListener("keyup", debounceInput); // check for "change"=> you have to wait and click outside or enter.
+movieRatings.addEventListener("change", handleRatingSelector); // check for "change"=> you have to wait and click outside or enter.
+moviesGenre.addEventListener("change", handleGenreSelect);
